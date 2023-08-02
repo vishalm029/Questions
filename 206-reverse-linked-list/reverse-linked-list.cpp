521c57482113aacb72bee784eed468b9becb3440
Vishal Mishra <128444479+vishalm029@users.noreply.github.com>
2023-08-02 07:32:23 +0530

    Time: 8 ms (31.72%) | Memory: 9.3 MB (12.66%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -10,20 +10,41 @@
  */
 class Solution {
 public:
+    void Reverse(ListNode* &head,ListNode* &curr,ListNode* &prev){
+        //base case
+        if(curr==NULL){
+            head=prev;
+            return;
+        }
+        ListNode* forward=curr->next;
+        Reverse(head,forward,curr);
+        curr->next=prev;
+
+    }
     ListNode* reverseList(ListNode* head) {
-        //case for null and 1 node list
-        if(head==NULL||head->next==NULL){
-            return head;
-        }
-        ListNode* prev=NULL;
-        ListNode* curr=head;
-        ListNode* forward=NULL;
-        while(curr!=NULL){
-            forward=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=forward;
-        }
-        return prev;
+
+/////////////////ITERATIVE SOLUTION/////////////////
+        // //case for null and 1 node list
+        // if(head==NULL||head->next==NULL){
+        //     return head;
+        // }
+        // ListNode* prev=NULL;
+        // ListNode* curr=head;
+        // ListNode* forward=NULL;
+        // while(curr!=NULL){
+        //     forward=curr->next;
+        //     curr->next=prev;
+        //     prev=curr;
+        //     curr=forward;
+        // }
+        // return prev;
+
+
+////////////RECURSIVE SOLUTION////////
+    ListNode* curr=head;
+    ListNode* prev=NULL;
+    Reverse(head,curr,prev);
+    return head;
+
     }
 };
