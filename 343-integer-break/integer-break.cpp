e8ce3f2c1894e8c4bd3f2a0dab0e05edb4c482b0
Vishal Mishra <128444479+vishalm029@users.noreply.github.com>
2023-08-03 15:56:52 +0530

    Time: 3 ms (44.97%) | Memory: 6.2 MB (54.57%) - LeetSync

--- 343-integer-break/integer-break.cpp
+++ 343-integer-break/integer-break.cpp
@@ -2,27 +2,41 @@
 
 class Solution {
 public:
+    int fun(int n ,vector<int> &dp){
+    if(n==0) return 1;
+    if(dp[n]!=-1) return dp[n];
+    int ans =n;
+    for(int i=1;i<n;i++){
+        ans =max(ans,i*fun(n-i,dp));
+    }
+    return dp[n]=ans ;
+}
     int integerBreak(int n) {
-        if(n<=3) return n-1;
-        
-        vector<vector<ll>> dp(n+1,vector<ll>(n+1, 1));
-        
-        for(int i=2;i<=n;i++){
-            for(int j=1;j<= n;j++){
-                
-                ll pro= INT_MIN;
-                if(j<=i){
-                    pro= dp[i-j][j]*j ;
-                }
-                dp[i][j]= max(pro, dp[i][j-1]);
-            }
-        }
-        
-        // for(auto &x:dp){
-        //     for(auto y:x)cout<<y<<" ";
-        //     cout<<"\n";
-        // }
-        
-        return dp[n][n];
+        if(n<4) return n-1;
+        vector<int> dp(n+1,-1);
+        return fun(n,dp);
     }
 };
+        // if(n<=3) return n-1;
+        
+        // vector<vector<ll>> dp(n+1,vector<ll>(n+1, 1));
+        
+        // for(int i=2;i<=n;i++){
+        //     for(int j=1;j<= n;j++){
+                
+        //         ll pro= INT_MIN;
+        //         if(j<=i){
+        //             pro= dp[i-j][j]*j ;
+        //         }
+        //         dp[i][j]= max(pro, dp[i][j-1]);
+        //     }
+        // }
+        
+        // // for(auto &x:dp){
+        // //     for(auto y:x)cout<<y<<" ";
+        // //     cout<<"\n";
+        // // }
+        
+        // return dp[n][n];
+        
+ 
