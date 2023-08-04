71b9afb10b41883028b74e534896b335f83c28b2
Vishal Mishra <128444479+vishalm029@users.noreply.github.com>
2023-08-05 00:36:18 +0530

    Time: 6 ms (75.88%) | Memory: 8.9 MB (70.35%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    //Recursion + Memoization
+    bool RecMemo(int ind, int n, vector<string> &words, string s, vector<int> &dp) {
+        if(ind == n){
+            return true;
+        }
+        if(dp[ind] != -1){
+            return dp[ind];
+        }
+
+        for(auto word: words) {
+            if(s.substr(ind, word.size()) == word and RecMemo(ind + word.size(), n, words, s, dp))
+                return dp[ind] = true;
+        }
+        
+        return dp[ind] = false;
+    }
+
+    bool wordBreak(string s, vector<string>& words) {
+        int n = s.size();
+        vector<int> dp(n, -1);
+        return RecMemo(0, n, words, s, dp);
+    }
+
+    // //Tabulation
+    // bool wordBreak(string s, vector<string>& words) {
+    //     int n = s.size();
+    //     vector<bool> dp(n + 1, 0);
+    //     dp[n] = true;
+
+    //     for(int i = n - 1; i >= 0; i--) {
+    //         for(auto word: words) {
+    //             if(i + word.size() <= n and s.substr(i, word.size()) == word and dp[i + word.size()])
+    //                 dp[i] = true;
+    //             if(dp[i]) break;
+    //         }
+    //     }
+
+    //   return dp[0];
+    //}
+};
