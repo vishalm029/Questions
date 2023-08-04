eb75d415309af5ef0b6972749461bb18f191f0b7
Vishal Mishra <128444479+vishalm029@users.noreply.github.com>
2023-08-04 14:59:40 +0530

    Time: 2 ms (46.54%) | Memory: 6.2 MB (34.70%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        stack<int> open ,star;
+        int n = s.size();
+        for(int i =0; s[i] != '\0' ; i++){
+            if(s[i]== '('){
+                open.push(i);
+            }
+            else if(s[i] == '*'){
+                star.push(i);
+            }
+            else{
+                if(!open.empty()){
+                    open.pop();
+                }
+                else if(!star.empty()){
+                    star.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+
+        while(!open.empty()){
+            if(star.empty())
+                return false;
+            else if(open.top() <star.top()){
+                open.pop();
+                star.pop();
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
