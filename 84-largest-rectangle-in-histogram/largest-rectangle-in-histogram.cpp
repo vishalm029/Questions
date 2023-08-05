746909620644587e67bae47165fde0889708ede5
Vishal Mishra <128444479+vishalm029@users.noreply.github.com>
2023-08-05 12:46:20 +0530

    Time: 156 ms (91.96%) | Memory: 93.5 MB (7.75%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,65 @@
+class Solution {
+private:
+    //Function for nextSmaller
+    vector<int> nextSmallerElement(vector<int> arr, int n) {
+        stack<int> s;
+        s.push(-1);
+        vector<int> ans(n);
+
+        for(int i=n-1; i>=0 ; i--) {
+            int curr = arr[i];
+            while(s.top() != -1 && arr[s.top()] >= curr)
+            {
+                s.pop();
+            }
+            //ans is stack ka top
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans;
+    }
+    //Function for prevSmaller
+    vector<int> prevSmallerElement(vector<int> arr, int n) {
+        stack<int> s;
+        s.push(-1);
+        vector<int> ans(n);
+
+        for(int i=0; i<n; i++) {
+            int curr = arr[i];
+            while(s.top() != -1 && arr[s.top()] >= curr)
+            {
+                s.pop();
+            }
+            //ans is stack ka top
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans; 
+    }
+    
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n= heights.size();
+        
+        vector<int> next(n);
+        next = nextSmallerElement(heights, n);
+            
+        vector<int> prev(n);
+        prev = prevSmallerElement(heights, n);
+        
+        int area = INT_MIN;
+        for(int i=0; i<n; i++) {
+             //Length is constant
+            int l = heights[i];
+            //If all heights of all bars are equal
+            if(next[i] == -1) {
+                next[i] = n;
+            }
+             //breadth is changing and hence we are calculating breadth by taking the breadth till next bar value is greater than previous 
+            int b = next[i] - prev[i] - 1;
+            int newArea = l*b;
+            area = max(area, newArea);
+        }
+        return area;
+    }
+};
