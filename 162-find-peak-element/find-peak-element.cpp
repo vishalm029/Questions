0847a51b850b2ac7251a557796c74e9850e3255f
Vishal Mishra <128444479+vishalm029@users.noreply.github.com>
2023-08-11 20:08:09 +0530

    Time: 3 ms (82.25%) | Memory: 8.9 MB (51.51%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,7 +1,27 @@
+//brute force
+// class Solution {
+// public:
+//     int findPeakElement(vector<int>& nums) {
+
+//         return max_element(nums.begin(), nums.end())-nums.begin();
+//     }
+// };
+
+
+
+//Binary Search
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-
-        return max_element(nums.begin(), nums.end())-nums.begin();
+        int s = 0 , e = nums.size()-1;
+        while(s<e) {
+            int mid = s + (e-s)/2;
+            if(nums[mid] < nums[mid + 1])
+            s = mid + 1;
+            else{
+                e=mid;
+            }
+        }
+        return s;
     }
 };
