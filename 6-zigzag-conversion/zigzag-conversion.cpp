7c1fd379920ada27c216bbcb464e99a6426af02d
Vishal Mishra <128444479+vishalm029@users.noreply.github.com>
2023-08-11 19:19:45 +0530

    Time: 6 ms (87.52%) | Memory: 10.4 MB (69.71%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -5,12 +5,12 @@ public:
         if(numRows==1){
             return s;
         }
-        vector<vector<char>> vec(numRows);
+        vector<string> vec(numRows,"");
         int dir=-1;
         int index=0;
         int Row_index=0;
         for(int i=0;i<n;i++){
-            vec[Row_index].push_back(s[i]);
+            vec[Row_index]+=s[i];
             if(Row_index==0 || Row_index==numRows-1){
                 dir*=-1;
             }
@@ -18,9 +18,7 @@ public:
         }
         string ans="";
         for(int i=0;i<numRows;i++){
-            for(int j=0;j<vec[i].size();j++){
-                ans+=vec[i][j];
-            }
+            ans+=vec[i];
         }
         return ans;
 
