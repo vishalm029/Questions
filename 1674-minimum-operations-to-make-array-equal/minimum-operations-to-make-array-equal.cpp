f97ddca0662e732df91d5fc617eceb202ce09569
Vishal Mishra <128444479+vishalm029@users.noreply.github.com>
2023-08-16 17:25:57 +0530

    Time: 0 ms (100.00%) | Memory: 6.1 MB (14.82%) - LeetSync

--- 1674-minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.cpp
+++ 1674-minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int minOperations(int n) {
-       int k=0,s=0;
-       //int i=n/2;
+       int s=0;
        int max=n;
         for(int j=0;j<n/2;j++){
            s+=max-2*j-1;
