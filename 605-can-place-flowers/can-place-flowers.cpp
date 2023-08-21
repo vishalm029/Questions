2c9ecf122750434dedd89688c1e1a1faa6dc965e
Vishal Mishra <128444479+vishalm029@users.noreply.github.com>
2023-08-21 21:40:08 +0530

    Time: 20 ms (35.76%) | Memory: 20.4 MB (26.55%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int ch=0;
+        for(int i=0;i<flowerbed.size();i++){
+            if(flowerbed[i]==1){
+                continue;
+            }
+            else{
+                if((i-1<0 || flowerbed[i-1]==0) &&
+                 (i+1==flowerbed.size() || flowerbed[i+1]==0)){
+                flowerbed[i]=1;
+                ch++;
+                }
+            }
+        }
+        cout<<ch<<endl;
+        if(ch>=n){
+            return true;
+        }
+        return false;
+        
+    }
+};
