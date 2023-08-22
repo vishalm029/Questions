c02d539110948fcfd061bc0dd13e4ecb788cf9ee
Vishal Mishra <128444479+vishalm029@users.noreply.github.com>
2023-08-22 16:54:11 +0530

    Time: 2 ms (49.78%) | Memory: 5.9 MB (79.56%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.cpp
+++ 168-excel-sheet-column-title/excel-sheet-column-title.cpp
@@ -4,16 +4,9 @@ public:
         int num;
         string ans;
         while(columnNumber>0){
+            --columnNumber;
             num=columnNumber%26;
-            if(num==0){
-
-                ans.push_back('Z');
-                columnNumber-=26;
-            }
-            else{
-                ans.push_back('A'+num-1);
-                columnNumber-=num;
-            }
+            ans.push_back('A'+num);
             columnNumber/=26;
         }
         reverse(ans.begin(),ans.end());
