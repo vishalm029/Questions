243f7c2a3328d296b1bb561bce2cef0840e0a5ab
Vishal Mishra <128444479+vishalm029@users.noreply.github.com>
2023-09-02 15:07:04 +0530

    Time: 676 ms (10.29%) | Memory: 196.3 MB (15.16%) - LeetSync

---
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    map<string,int>m;
+    int character(int i,string &s,vector<int>&dp){
+        if(i>=s.size())return 0;
+        int ans=INT_MAX;
+        if(dp[i]!=-1)return dp[i];
+        ans = 1 + character(i+1,s,dp);
+        string k="";
+        for(int j=i;j<s.size();j++){
+            k+= s[j];
+            if(m[k]){
+                ans=min(ans,character(j+1,s,dp));
+            }
+        }
+        return dp[i]=ans;
+    }
+    int minExtraChar(string s, vector<string>& d) {
+        for(auto i : d)m[i]++;
+        vector<int>dp(s.size(),-1);
+        return character(0,s,dp);
+    }
+};
