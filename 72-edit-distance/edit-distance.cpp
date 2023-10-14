2933230682df723f29d83c92d3a42f625e4a231b
Vishal Mishra <128444479+vishalm029@users.noreply.github.com>
2023-10-15 00:56:11 +0530

    Time: 3 ms (97.39%) | Memory: 9.5 MB (25.31%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,16 +1,17 @@
 class Solution {
-public:
-    int dfs(int i, int j, string s, string t, vector<vector<int>>& dp) {
-        if (i == 0) return j;
-        if (j == 0) return i;
-        if (dp[i][j] != -1) return dp[i][j];
-        if (s[i-1] == t[j-1]) return dp[i][j] = 0 + dfs(i-1, j-1, s, t, dp);
-        return dp[i][j] = 1 + min(dfs(i, j-1, s, t, dp), min(dfs(i-1, j, s, t, dp), dfs(i-1, j-1, s, t, dp)));
+    int dfs(int i, int j, string &s, string &t, vector<vector<int>> &dp) {
+        if(j == 0) return i;
+        if(i == 0) return j;
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        if(s[i - 1] == t[j - 1]) return dp[i][j] = dfs(i - 1, j - 1, s, t, dp);
+        return dp[i][j] = 1 + min(dfs(i - 1, j - 1, s, t, dp), min(dfs(i, j - 1, s, t, dp), dfs(i - 1, j, s, t, dp)));
     }
-    int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return dfs(n, m, word1, word2, dp);
+public:
+    int minDistance(string s, string t) {
+        int n = s.size(), m = t.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+
+        return dfs(n, m, s, t, dp);
     }
 };
