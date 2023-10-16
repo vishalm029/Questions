59ea72970ceb164cf999cd1701c3605711252e1f
Vishal Mishra <128444479+vishalm029@users.noreply.github.com>
2023-10-16 20:11:21 +0530

    Time: 53 ms (8.33%) | Memory: 13 MB (61.67%) - LeetSync

--- 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.cpp
+++ 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.cpp
@@ -1,22 +1,50 @@
+
 class Solution {
+    vector<int> dir = { 0,-1, 1,0, 0,1, -1,0 } ;
 public:
-    vector<vector<int>> rotateGrid(vector<vector<int>>& g, int k) {
-        int top = 0, l = 0, btm = g.size()-1, r = g[0].size()-1;
-        while(top < btm && l < r) {
-            int total = 2*(btm-top)+2*(r-l), shift = k%total;
-            while(shift-- > 0) {
-                int last = g[top][l];
-                for(int c = l; c < r; c++) g[top][c] = g[top][c+1];
-                for(int rr = top; rr < btm; rr++) g[rr][r] = g[rr+1][r];
-                for(int c = r; c > l; c--) g[btm][c] = g[btm][c-1];
-                for(int rr = btm; rr > top+1; rr--) g[rr][l] = g[rr-1][l];
-                g[top+1][l] = last;
-            }
-            top++;
-            btm--;
-            l++;
-            r--;
+    vector<vector<int>> rotateGrid(vector<vector<int>>& grid, int k) {
+        
+        int n = grid.size() ;
+        int m = grid[0].size() ;
+        
+        int rLL=0,rUL = n-1,cLL = 0, cUL = m-1 ;
+        
+        while(rLL<=rUL && cLL <= cUL ){
+            
+            int k2 = k%( (rUL-rLL-1)*2 + 2*(cUL-cLL+1) ) ;
+            
+            for(int i=0;i<k2;i++)
+                move(rLL,rUL,cLL,cUL,grid) ;
+            
+            rLL++;rUL--;
+            cLL++;cUL--;
         }
-        return g;
+        
+        return grid;
+    }
+    
+    void move(int rLL,int rUL,int cLL,int cUL,vector<vector<int>>& grid){
+       
+        int j2 = cUL;
+        int i2 = rLL ;
+        
+        int d = 0 ;
+        int prev=-1;
+        
+        do{
+            swap(grid[i2][j2], prev) ; 
+            
+            int i3 = i2+dir[d],j3 =j2+dir[d+1] ;
+            
+            if( j3 < cLL || i3 < rLL || j3 > cUL || i3 > rUL )
+                d+=2 ;
+                    
+            i2 = i2+dir[d] ;
+            j2 = j2+dir[d+1] ;
+            
+        }while( (i2 != rLL) || (j2 != cUL ) ) ;
+        
+        grid[i2][j2] = prev ;
+        
     }
 };
