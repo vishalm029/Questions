11c0afba5dd3ba7b201bf86fbda3047365538b07
Vishal Mishra <128444479+vishalm029@users.noreply.github.com>
2023-12-21 13:42:22 +0530

    Time: 1404 ms (9.37%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,9 +1,19 @@
+# SELECT
+#     p.project_id,
+#     ROUND(AVG(em.experience_years), 2) AS average_years
+# FROM
+#     Project p
+#  LEFT JOIN
+#     Employee em ON p.employee_id = em.employee_id
+# GROUP BY
+#     p.project_id;
 SELECT
     p.project_id,
     ROUND(AVG(em.experience_years), 2) AS average_years
 FROM
     Project p
- LEFT JOIN
+JOIN
     Employee em ON p.employee_id = em.employee_id
 GROUP BY
     p.project_id;
+
